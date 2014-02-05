75a6dfeec8e1f70bd4dac743b20031a931be17e6
unknown <paolo.freuli@gmail.com>
2014-02-05 22:42:11 +0100

    fixes on emit

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
+var loglevel= 'debug'
+var Logme = require('logme').Logme;
+var logme = new Logme({ level: loglevel });
+
 var packet =require('mqtt_packet');
 var server_handler = require('./lib/server/server_handler.js');
 
 //var client_handler = require('./lib/client/client_handler.js');
 server_handler.builtin_handlers={
 	'subscribe_received_event' : false,
-	'suback_sent_event' : false
+	'suback_sent_event' : true,
+	'subscribe_received_event' : false,
+	'connect_decoded_msg' : false,
 	};
 
-server_handler.builtin_handlers['suback_sent_event']= false;
-server_handler.bubble_events = false; 
+server_handler.builtin_handlers['suback_sent_event']= true;
+server_handler.bubble_events = true; 
 	
-server_handler.on('suback_sent_event', function(socket) {
-	console.log(socket);
+server_handler.on('suback_sent_event', function(params) {
+		logme.inspect(params);
 });
 
 
